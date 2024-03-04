78cef8b54d44ad981d94b0d48f717556590e8bf8
autumn808 <AutumnCapasso@gmail.com>
2024-03-04 10:19:37 -0800

    added comments to explain functionality to students

--- src/BiblioVaultApp.java
+++ src/BiblioVaultApp.java
@@ -10,13 +10,16 @@ public class BiblioVaultApp extends JFrame implements ActionListener {
     }
 
     public static void main(String[] args) {
+        //This is where we create our applications frame(window) to put in all our components
         JFrame frame = new JFrame("Biblio Valut");
-
         JPanel panel = new JPanel();
+        //setting the window and background layout
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        //changing the color
         panel.setBackground(java.awt.Color.pink);
 
 
+        //This is where we will add the text box for Book title (JTextFeild) and the label for the text box, also set size and alignment
         bookTitle = new JTextField(20);
         bookTitle.setMaximumSize( new java.awt.Dimension(  300, 100) );
         JLabel bookLabel = new JLabel("Book Title:");
@@ -25,7 +28,7 @@ public class BiblioVaultApp extends JFrame implements ActionListener {
         panel.add(bookLabel);
         panel.add(bookTitle);
 
-
+        //Doing the same thing for ISBN number and each text box we create
         isbnText = new JTextField(20);
         isbnText.setMaximumSize( new java.awt.Dimension(  300, 100) );
         JLabel isbnLabel = new JLabel("ISBN:");
@@ -52,13 +55,16 @@ public class BiblioVaultApp extends JFrame implements ActionListener {
         genreLabel.setAlignmentX(panel.CENTER_ALIGNMENT );//0.0
         genreText.setAlignmentX(panel.LEFT_ALIGNMENT );//0.0
 
+        //Here we create our button to submit
         submitButton = new JButton("Submit");
         panel.add(submitButton);
         submitButton.addActionListener(new BiblioVaultApp());
 
+        //The result label will show the data entered
         resultLabel = new JLabel("Nothing entered");
         panel.add(resultLabel);
 
+        //This is where we set up the size and specifications of the frame
         frame.add(panel);
         frame.setSize(500, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -66,7 +72,8 @@ public class BiblioVaultApp extends JFrame implements ActionListener {
 
 
     }
-
+    /* actionPerformed tells the compenents in your frame what actions to execute this is how
+     this is how you add functionality to things like buttons. */
     public void actionPerformed(ActionEvent e) {
         String s = e.getActionCommand();
         if (s.equals("Submit")) {
